ff2e90a46a4db839bcf5f77216810d9d6fb2cfda
Mesgun <Mesgun@MESGUN->
2026-02-24 17:39:33 +0100

    Models/Enum classs

---
+++ Hotel App/Models/Enums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_App.Models
+{
+    internal class Enums
+    {
+        public enum RoomType { Single = 1, Double = 2 }
+        public enum BookingStatus { Active = 1, Cancelled = 2 }
+    }
+}
