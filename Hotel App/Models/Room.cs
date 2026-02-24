85effa94fd3b2551db58968a6d5ef0b3a831548d
Mesgun <Mesgun@MESGUN->
2026-02-24 18:02:07 +0100

    Uppdatera Room class

--- Hotel App/Models/Room.cs
+++ Hotel App/Models/Room.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System.Collections.Generic;
+
+namespace Hotel_App.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string RoomNumber { get; set; } = "";
+        public RoomType Type { get; set; }
+        public int BaseCapacity { get; set; }
+        public int ExtraBedsMax { get; set; }
+        public decimal PricePerNight { get; set; }
+
+        public List<Booking> Bookings { get; set; } = new();
+    }
+}
