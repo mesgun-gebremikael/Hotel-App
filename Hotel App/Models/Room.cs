d78c985f90b5fbcb1dbf0d9668bf69e8de651b70
Mesgun <Mesgun@MESGUN->
2026-02-24 17:44:36 +0100

    Roms class la till ID,rommnummer,typ price, booking osv

--- Hotel App/Models/Room.cs
+++ Hotel App/Models/Room.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Hotel_App.Models.Enums;
 
 namespace Hotel_App.Models
 {
-    internal class Room
-    {
-    }
+    
+    
+        public class Room
+        {
+            public int Id { get; set; }
+            public string RoomNumber { get; set; } = "";
+            public RoomType Type { get; set; }
+            public int BaseCapacity { get; set; }
+            public int ExtraBedsMax { get; set; }
+            public decimal PricePerNight { get; set; }
+
+            public List<Booking> Bookings { get; set; } = new();
+        }
+    
 }
